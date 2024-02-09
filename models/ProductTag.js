ea518685e1c8d98f1d40a9c1cbc3a2aedbfccd31
davidmelendez <Dmelz11@github.com>
2024-02-09 12:59:47 -0800

    corrected path and two sntax issues in ProductTag file in models folder

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../Develop/config/connection');
+const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
@@ -15,14 +15,14 @@ ProductTag.init(
   product_id:{
     type: DataTypes.INTEGER,
     references: {
-      model: "Product",
+      model: "product",
       key: "id",
     }
   },
   tag_id: {
     type: DataTypes.INTEGER,
     references: {
-      model: "Tag",
+      model: "tag",
       key: "id",
     }
   },
